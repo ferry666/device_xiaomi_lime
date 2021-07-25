2507ffdc33036e9d38c85a7b42dd7c53364b4373
isnrdra <isanoorendra@gmail.com>
2021-07-25 07:07:48 +0000

    juice: ConquerOS Bringup!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_juice.mk
+    $(LOCAL_DIR)/conquer_juice.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_juice-user \
-    aosp_juice-userdebug \
-    aosp_juice-eng
+    conquer_juice-user \
+    conquer_juice-userdebug \
+    conquer_juice-eng
