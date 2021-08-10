1408af9629cae01e9053982a42bc24788147826e
isnrdra <65233608+isnrdra@users.noreply.github.com>
2021-08-10 18:20:21 +0700

    juice: AmyROM Bringup

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -12,14 +12,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
 # Inherit some common aosp stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/amy/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_BLUR := true
 USE_PIXEL_CHARGING := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_juice
+PRODUCT_NAME := amy_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
@@ -30,6 +30,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 #Fingerprint
 BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
 BUILD_DESCRIPTION := redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys
-
-#PixelBlaster
-BLASTER_BUILD_TYPE := OFFICIAL
