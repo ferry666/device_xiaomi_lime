50622a86163b6a36f8aace2e82b459a39d396650
isnrdra <isanoorendra@gmail.com>
2021-08-17 11:23:01 +0000

    Juice: NusantaraOS Bringup

---
+++ nad_juice.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit framework first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+$(call inherit-product, device/xiaomi/juice/device.mk)
+
+# Inherit some common NusantaraOS stuff.
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_USES_BLUR := true
+USE_PIXEL_CHARGING := true
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := nad_juice
+PRODUCT_DEVICE := juice
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := sm6115
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+#Fingerprint
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
+BUILD_DESCRIPTION := redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys
