e117a8734c7c364d5c978ba54fb589aae2107fc8
inrdra <65233608+inrdra@users.noreply.github.com>
2021-09-08 16:59:50 +0700

    Juice: Nuke Fingerprint Override
    * PixelBlaster source had already implemented some other way.

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -27,11 +27,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-#Fingerprint
-PRODUCT_BUILD_PROP_OVERRIDES += \
-	PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001 7511028 release-keys"
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys
-
 #PixelBlaster
 BLASTER_BUILD_TYPE := OFFICIAL
 
