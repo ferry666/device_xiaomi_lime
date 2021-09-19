0e4f2955c4c94e6a981a05e3b439010d55757ab8
Mr.FF <40267376+ferry666@users.noreply.github.com>
2021-09-19 13:35:41 +0700

    Juice: bring up lineage os

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -7,18 +7,18 @@
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
 # Inherit some common nusantara stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_BLUR := true
 USE_PIXEL_CHARGING := true
 
 # Gapps
-#WITH_GAPPS := true
+#WITH_GAPPS := false
 #TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_juice
+PRODUCT_NAME := lineage_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
