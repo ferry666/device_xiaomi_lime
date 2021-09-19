a261b8f00d353a9e35ec5bb65f90a88de847e03b
Mr.FF <40267376+ferry666@users.noreply.github.com>
2021-09-19 13:38:46 +0700

    Juice: change aosp to lineage

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_juice.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_juice-user \
-    aosp_juice-userdebug \
-    aosp_juice-eng
+    lineage_juice-user \
+    lineage_juice-userdebug \
+    lineage_juice-eng
